63887e8a14138a7ef2155341f2df607077ea4eeb
Stévan Corre <stevancorre@protonmail.com>
2022-02-13 21:26:22 +0100

    Input file reading

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CFLAGS=-Wall -Wextra -pedantic -std=c11 -ggdb
+CLIBS=-lm
 
 .PHONY: all
 all: src/main.c
-	$(CC) $(CFLAGS) -o build/img2ppm src/main.c
+	$(CC) $(CFLAGS) ${CLIBS} -o build/img2ppm src/main.c
--- src/main.c
+++ src/main.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <errno.h>
 
-int main(void) {
-    printf("Ok\n");
-    
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+int main(int argc, char** argv) {
+    (void)argc;
+
+    assert(*argv != NULL);
+    const char *program_name = *argv++;
+
+    if (*argv == NULL)
+    {
+        fprintf(stderr, "Usage: %s <input image>\n", program_name);
+        fprintf(stderr, "ERROR: no args provided\n");
+        exit(1);
+    }
+
+    const char *input_file_path = *argv++;
+    int width, height, comp;
+
+    stbi_uc* data = stbi_load(input_file_path, &width, &height, &comp, 0);
+    if(data == NULL) {
+        fprintf(stderr, "ERROR: can't read %s: %s\n", input_file_path, strerror(errno));
+        exit(0);
+    }
+
+//     N=#comp     components
+//       1           grey
+//       2           grey, alpha
+//       3           red, green, blue
+//       4           red, green, blue, alpha
+
+    printf("comp: %u\n", comp);
+
     return 0;
 }
